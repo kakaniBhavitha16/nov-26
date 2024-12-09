63495877a546af33b34281c0bb395e9909922c1e
kakaniBhavitha16 <kakanibhavitha8@gmail.com>
2024-12-09 17:05:04 +0530

    loops

---
+++ index.js
@@ -0,0 +1,34 @@
+//task1
+
+for(var i=1; i<=20;i++){
+    console.log(i)
+}
+
+//task2
+for(i=1;i<=10;i++){
+    console.log(`square of ${i} is: ${i*i}`);
+}
+
+
+//task3
+for(var i; i<=10;i++){
+    console.log(`5 * ${i} is: ${i*5}`);
+}
+
+
+//task4
+for(var i=20; i>0; i--){
+console.log(i);
+}
+
+//task5
+for(i=20; i>=2; i=i-2){
+    console.log(i)
+}
+
+
+//task6
+var arr = [10, 20, 30, 40, 50];
+for (var i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
